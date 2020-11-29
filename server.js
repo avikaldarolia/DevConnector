dd678032ecd6dee63c413b6a36ff893a9b07587f
Avikal Darolia <avikaldarolia7@gmail.com>
2020-11-29 18:59:28 +0530

    Ready for Production

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express  = require('express');
 const connectDB = require('./config/db');
 const cors = require('cors');
-
+const path = require('path')
 //syntax for express. app refers to the name of whole backend application 
 const app = express();
 
@@ -11,14 +11,20 @@ app.use(cors());
 //Init Middleware - (Format for parsing data from API)
 app.use(express.json({extended: false}));
 
-// Kind of read only 
-app.get('/', (req, res) => res.send('API Running')); 
 
 //DEFINE routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+  
+    app.get('*', (req, res) =>
+      res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'))
+    );
+  }
 
 const PORT = process.env.PORT || 5000;
 
