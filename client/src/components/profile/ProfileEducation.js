9df9dd379e6bd1ca76ea7fc674683dd92adbd8d8
Avikal Darolia <avikaldarolia7@gmail.com>
2020-12-05 22:52:52 +0530

    Fixed Date Bug

--- client/src/components/profile/ProfileEducation.js
+++ client/src/components/profile/ProfileEducation.js
@@ -2,13 +2,14 @@ import React from "react";
 import PropTypes from "prop-types";
 import Moment from "react-moment";
 const ProfileEducation = ({
-  education: { school, degree, fieldofstudy, current, to, from, description },
+  education: { school, degree, fieldofstudy, to, from, description },
 }) => (
   <div>
     <h3 className="text-dark">{school}</h3>
     <p>
-      <Moment format="YYYY/MM/DD"> {from} </Moment> -{" "}
-      {!to ? "Now" : <Moment format="YYYY/MM/DD">{to} </Moment>}
+      
+      <Moment format="YYYY/MM/DD">{from}</Moment> -{" "}
+      {!to ? "Now" : <Moment format="YYYY/MM/DD">{to}</Moment>}
     </p>
     <p>
       <strong>Degree</strong> {degree}
@@ -22,7 +23,7 @@ const ProfileEducation = ({
   </div>
 );
 ProfileEducation.propTypes = {
-  education: PropTypes.array.isRequired,
+  education: PropTypes.object.isRequired,
 };
 
 export default ProfileEducation;
